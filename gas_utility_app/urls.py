70cd795f749643a71f9bf11d7fc446b7c7394c6a
saylijawale2001 <saylijawale2001@gmail.com>
2024-03-13 15:09:25 +0530

    better frontend

--- gas_utility_app/urls.py
+++ gas_utility_app/urls.py
@@ -10,16 +10,11 @@ urlpatterns = [
     path('customer/service_request_form/', views.service_request_form, name='service_request_form'),
     path('customer/dashboard/', views.dashboard, name='dashboard'),
     path('customer/others/', views.Customer_home, name='others'),
-    
-    
-
 
     path('staff/dashboard/',views.staff_dashboard,name='staff-dashboard'),
     path('staff/edit_request/<int:id>',views.edit_request,name='edit_request'),
 
-
     # path('submit-service-request/', views.service_request_submit, name='submit_service_request'),
-
     path('account-information/', views.account_info, name='account_information'),
     path('logout/', user_logout, name='logout'),
 ]
